ae45124f7b14d0ba56c19d8ae69f20c84c38d21b
Andrew Cortner <andrewcortner2021@u.northwestern.edu>
2020-01-30 20:03:24 -0600

    renders

--- chorus/src/App.js
+++ chorus/src/App.js
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import { Card } from "rbx";
 import './App.css';
 
-function App() {
+const App = () => {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    setData({"0": {"name": "Khalid", "art": "images/img_name.jpg", "comps": ["NONAME", "Daniel Caesar", "Frank Ocean"], "comps_img": ["img1.jpg", "img2.jpg", "img3.jpg"],"description": "This can be any string of text, as long as it stays in quotes.", "match_index": 35}});
+  })
+  const artists = Object.values(data);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div class="artist_container">
+        {artists.map(artist => <ArtistCard artist={artist}/>)}
+      </div>
     </div>
   );
+}
+
+const ArtistCard = ({artist}) => {
+  return (
+    <div>
+      <Card>
+        <h1 id="name">{artist.name}</h1>
+        <p id="play_button">&#9658;</p>
+      </Card>
+    </div>
+  )
 }
 
 export default App;
