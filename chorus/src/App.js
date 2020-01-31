fb1b8bd5c52fb00a9517c76b2d98b5f53f5f436a
Andrew Cortner <andrewcortner2021@u.northwestern.edu>
2020-01-30 20:37:01 -0600

    andrew's progress

--- chorus/src/App.js
+++ chorus/src/App.js
@@ -1,15 +1,16 @@
 import React, { useEffect, useState } from 'react';
-import { Card } from "rbx";
+import { Message, Card } from "rbx";
 import './App.css';
 
 const App = () => {
   const [data, setData] = useState([]);
   useEffect(() => {
-    setData({"0": {"name": "Khalid", "art": "images/img_name.jpg", "comps": ["NONAME", "Daniel Caesar", "Frank Ocean"], "comps_img": ["img1.jpg", "img2.jpg", "img3.jpg"],"description": "This can be any string of text, as long as it stays in quotes.", "match_index": 35}});
+    setData({"0": {"name": "Khalid", "art": "khalid.png", "comps": ["NONAME", "Daniel Caesar", "Frank Ocean"], "comps_img": ["img1.jpg", "img2.jpg", "img3.jpg"],"description": "This can be any string of text, as long as it stays in quotes.", "match_index": 35}});
   })
   const artists = Object.values(data);
   return (
     <div className="App">
+      <h1 id="banner">Artists you may like...</h1>
       <div class="artist_container">
         {artists.map(artist => <ArtistCard artist={artist}/>)}
       </div>
@@ -21,8 +22,12 @@ const ArtistCard = ({artist}) => {
   return (
     <div>
       <Card>
-        <h1 id="name">{artist.name}</h1>
-        <p id="play_button">&#9658;</p>
+        <img id="artwork" src={"./data/images/" + artist.art}/>
+        <div id="album_container">
+          <h1 id="artist_name">{artist.name}</h1>
+          <p id="play_btn">&#9658;</p>
+          
+        </div>
       </Card>
     </div>
   )
