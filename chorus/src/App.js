25109ee4ec23a1ac0423ce9eabf321f16e4e695b
Andrew Cortner <andrewcortner2021@u.northwestern.edu>
2020-01-30 21:01:06 -0600

    whoops my b

--- chorus/src/App.js
+++ chorus/src/App.js
@@ -1,12 +1,12 @@
 import React, { useEffect, useState } from 'react';
-import { Message, Card } from "rbx";
+import { Card } from "rbx";
 import './App.css';
 
 const App = () => {
   const [data, setData] = useState([]);
   useEffect(() => {
-    setData({"0": {"name": "Khalid", "art": "khalid.png", "comps": ["NONAME", "Daniel Caesar", "Frank Ocean"], "comps_img": ["img1.jpg", "img2.jpg", "img3.jpg"],"description": "This can be any string of text, as long as it stays in quotes.", "match_index": 35}});
-  })
+    setData({"0": {"name": "Khalid", "art": "khalid.png", "comps": [{"name": "Anderson Paak", "pic": "Anderson_Paak.jpg"}, {"name": "Anderson Paak", "pic": "Anderson_Paak.jpg"}, {"name": "Anderson Paak", "pic": "Anderson_Paak.jpg"}], "description": "This can be any string of text, as long as it stays in quotes.", "match_index": 35}});
+  }, []);
   const artists = Object.values(data);
   return (
     <div className="App">
@@ -26,11 +26,22 @@ const ArtistCard = ({artist}) => {
         <div id="album_container">
           <h1 id="artist_name">{artist.name}</h1>
           <p id="play_btn">&#9658;</p>
-          
+          <h2 id="similar">Similar Artists</h2>
+          <div class="related_artists">{artist.comps.map(comp => <ArtistComp comp={comp}/>)}</div>
         </div>
       </Card>
     </div>
   )
 }
+
+const ArtistComp = ({comp}) => {
+  return (
+    <div class="comp_wrapper">
+      <img id="comp_pic" src={"./data/images/" + comp.pic}/>
+      <h3 id="comp_name">{comp.name}</h3>
+    </div>
+  )
+}
+
 
 export default App;
